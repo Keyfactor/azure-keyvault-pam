486f177e9da3f33026714f20f121a630058bda74
Matthew Irby <matt.irby@outlook.com>
2025-02-05 10:04:40 -0500

    Add test console

---
+++ TestConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Keyfactor.Extensions.Pam.AzureKeyVault;
+
+namespace TestConsole;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        KeyVaultPam pam = new();
+        Dictionary<string, string> initializationInfo = new();
+        Dictionary<string, string> instanceParameters = new();
+        
+        initializationInfo.Add("KeyVaultUri", Environment.GetEnvironmentVariable("KEYVAULT_VAULT_URI") ?? string.Empty);
+        instanceParameters.Add("SecretId", Environment.GetEnvironmentVariable("KEYVAULT_SECRET_ID") ?? string.Empty);
+
+        string password = pam.GetPassword(instanceParameters, initializationInfo);
+        Console.WriteLine($"Password from Azure KeyVault: {password}");
+    }
+}
